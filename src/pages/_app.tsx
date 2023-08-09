61a0d6d6a77a6e8d8e522e355f3acf09fe2a3167
gemtunde <gemtunde@gmail.com>
2023-08-09 07:43:59 +0100

    added show password, toastify and bg-image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({
   Component,
@@ -9,6 +11,18 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <Head>
         <title> Nextjs Authentication App - gemtunde</title>
       </Head>
